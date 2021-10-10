6b344eba19d59777394f16a57751023f0acdc07f
unknown <knaub_elena@yahoo.com>
2021-10-10 18:04:50 +0300

    xp-gold

--- Week2/Day1/Class Exercise/script.js
+++ Week2/Day1/Class Exercise/script.js
@@ -1,7 +1,25 @@
-let addressNumber = 23;
-let addressStreet = " Wurstgasse";
-let country = " Austra";
+// let addressNumber = 23;
+// let addressStreet = " Wurstgasse";
+// let country = " Austra";
+//
+//
+// let globalAddress = "I live at "+ addressNumber + addressStreet + country;
+// console.log(globalAddress)
 
 
-let globalAddress = "I live at "+ addressNumber + addressStreet + country;
-console.log(globalAddress)
+
+//
+// ----- exercise 3: animal array
+// 1. Create a numerically indexed table (ie. an array): pets, like this ['cat','dog','fish','rabbit','cow']
+//
+// 2. Display dog
+//
+// 3. Add to the array pets, the pet horse. Remove the pet cow
+//
+// 4. Display the array length
+//
+// let pets = ['cat','dog','fish','rabbit','cow'];
+// console.log(pets[1]);
+// pets.push("horse");
+// pets.pop();
+// console.log(pets.length);
---
+++ Week2/Day1/Extra Exercise/jp-gold.js
@@ -0,0 +1,39 @@
+///Exercise 1
+
+let me = ["my","favorite","color","is","blue"];
+console.log(me.join(" "));
+
+
+///Exercise 2
+
+let str1 = "mix";
+let str2 = "pod";
+
+let new1 = str1.replace("x", "d");
+let new2 = str2.replace("d", "x");
+let space = " ";
+let newphrase = str2.concat(space, str1);
+console.log(newphrase);
+
+
+///Exercise 3
+
+let num1 = parseInt(prompt("Pick a first number"));
+let num2 = parseInt(prompt("Pick a second number"));
+alert(num1 + num2);
+
+
+///3 Bonus
+
+let num1 = parseInt(prompt("Pick a first number"));
+let num2 = parseInt(prompt("Pick a second number"));
+let calc = (prompt("What do you want to do? + - / or *"));
+if (calc === "+") {
+    alert(num1 + num2);
+}
+else if (calc === "*") {
+    alert(num1 * num2);
+}
+else  {
+    alert(num1 - num2);
+}
