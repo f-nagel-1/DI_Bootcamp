0fa427cfabaeb7a1616188fe7314d1fbe4ac074b
unknown <knaub_elena@yahoo.com>
2021-10-11 13:55:08 +0300

    exercise xp 11.10

--- Week2/Day2/Homework/exercise-xp.js
+++ Week2/Day2/Homework/exercise-xp.js
@@ -49,3 +49,39 @@ if (newDog === "Chihuahua"){
 // Check whether the variable is even or odd.
 // If it is even, display: “x is an even number”. Where x is the actual number the user chose.
 // If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+
+var number = prompt("Give me a number");
+
+if (number%2 == 0){
+    console.log(number , "is an even number");
+}else{
+    console.log(number , "is an uneven number");
+}
+
+
+
+// Exercise 4: Group Chat
+// Instructions
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+// Using the array above, console.log the number of users in a group chat based on the following rules:
+// If there is no users (the users array is empty), console.log “no one is online”.
+// If there is 1 user, console.log “<name_user> is online”.
+// If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
+// If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+// For example, if there are 5 users, it should display:
+// name_user1, name_user2 and 3 more are online
+
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+var countNumber = users.length;
+// console.log(countNumber)
+
+if (countNumber === 0){
+    console.log("no one is online")
+} else if (countNumber === 1){
+    console.log(users[0], "is online")
+} else if (countNumber === 2){
+    console.log(users[1], "is online")
+} else {
+    console.log(users[0], users[1], "and 2 more users are online")
+}
