390b72c781c0dfdd1efb383d7d9e8fba8b6fb28b
unknown <knaub_elena@yahoo.com>
2021-11-07 12:31:13 +0200

    daily challenge

---
+++ Week6/Day1/Homework/daily_challenge1.sql
@@ -0,0 +1,2 @@
+insert into actors(first_name, last_name, age, number_oscars)
+values('Indiana', '', '01/02/2000', '')
---
+++ Week6/Day1/Homework/daily_challenge2.sql
@@ -0,0 +1,4 @@
+insert into actors(first_name, age)
+values('Indiana', '01/02/2000')
+
+--since all the columns are mandatory (not null) empty fields cannot be inserted
